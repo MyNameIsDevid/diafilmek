fcea8845da048d6b0d3867ebeec210529e9b97b9
MyNameIsDevid <hdevid12@gmail.com>
2022-04-05 20:09:16 +0200

    eleje kesz

---
+++ client/fooldal.php
@@ -0,0 +1,17 @@
+<div class="container">
+    <h2 class="text-center mb-5">A diafilmekről</h2>
+    <div class="row">
+        <div class="col-md-8 col-lg-8 col-sm-8">
+            <p class="text-justify" style="text-indent: 10px;">
+                Egy diafilm legfeljebb 200 darab 18×24 milliméteres „kockát” tartalmazó filmszalag, amely az erre alkalms diavetítő segítségével megvilágítva fehér felületre kivetíthető.
+            </p>
+            <p class="text-justify" style="text-indent: 10px;">
+                Magyarországon diafilmszalagokat először a Székesfővárosi Pedagógiai Filmgyár kezdett gyártani, s 1913 és 1931 között több, színes illusztrációs anyagot tartalmazó oktatófilmet is kiadott ebben a formában. Ezek a gyakran hosszú filmtekercsek általában 80–190 darab 18×24 milliméteres kockát tartalmaztak, amelyhez a szöveges információt inzertkockák biztosították.
+            </p>
+
+        </div>
+        <div class="col-md-4 col-lg-4 col-sm-4">
+            <img src="diafilm.jpg" class="img-fluid" alt="">
+        </div>
+    </div>
+</div>
---
+++ client/index.php
@@ -0,0 +1,62 @@
+﻿<!DOCTYPE html>
+<html lang="hu">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
+  <title>Diafilmek</title>
+</head>
+
+<body>
+  <div class="container bg-light p-0">
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark rounded shadow ">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item p-2 m-2">
+            <a class="nav-link" href="http://localhost/diafilmek/client/index.php?program=fooldal.php">Főoldal</a>
+          </li>
+          <li class="nav-item p-2 m-2">
+            <a class="nav-link" href="http://localhost/diafilmek/client/index.php?program=statisztika.php">Statisztika</a>
+          </li>
+          <li class="nav-item p-2 m-2">
+            <a class="nav-link" href="http://localhost/diafilmek/client/index.php?program=kiadok.php">Kiadók</a>
+          </li>
+          <li class="nav-item p-2 m-2">
+            <a class="nav-link" href="http://localhost/diafilmek/client/index.php?program=ujkiado.php">Új kiadó</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="row p-3 justify-content-center">
+      <!--az URL-ben kapott program betöltése-->
+      <?php
+      if (isset($_GET['program'])) {
+          include $_GET['program'];
+      } else
+          include("fooldal.php");
+      ?>
+    </div>
+
+  </div>
+ 
+  <script src="bootstrap/jquery-3.5.1.min.js"></script>
+  <script src="bootstrap/bootstrap.bundle.js"></script>
+  <script>
+    let navs = document.getElementsByClassName("nav-link");
+
+      for(let i = 0; i < navs.length; i++){
+          if(navs[i].href == document.URL){
+            navs[i].style.color = "orange";
+          }
+      }
+
+  </script>
+</body>
+
+</html>
